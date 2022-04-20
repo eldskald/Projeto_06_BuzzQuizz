81f5b905b1170a9bcd5b511ad49b375d59eb6b8e
eldskald <rafaelbordoni00@gmail.com>
2022-04-20 17:54:58 -0300

    Limpa o código

--- js/tela-3.js
+++ js/tela-3.js
@@ -1,11 +1,14 @@
 //Essa funçao faz com que a tela 1 saia da tela e ainda vai adicionar a tela 3
 
 function carregaTela3(){
-   const aux1 = document.querySelector(".lista-de-quizzes").classList
-   const aux2 = document.querySelector(".CriarQuizzouQuizzCriados").classList
-   aux1.add("escondido");
-   aux2.add("escondido");
-   telaInfoBasica();
+//    const aux1 = document.querySelector(".lista-de-quizzes").classList
+//    const aux2 = document.querySelector(".CriarQuizzouQuizzCriados").classList
+//    aux1.add("escondido");
+//    aux2.add("escondido");
+//    telaInfoBasica();
+
+    limparMain();
+    telaInfoBasica();
 } 
 
 function verifTitulo(titulo){
