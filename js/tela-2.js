93d3780a23ca1d0cab24c88652842c02fbd50ac1
eldskald <rafaelbordoni00@gmail.com>
2022-04-20 21:10:58 -0300

    Adiciona tela 2

--- js/tela-2.js
+++ js/tela-2.js
@@ -8,13 +8,42 @@ function carregarTela2 (id) {
 
 function renderizarQuiz (quiz) {
     MAIN.innerHTML += renderizarTituloDoQuiz(quiz);
+    for (let i = 0; i < quiz.questions.length; i++) {
+        MAIN.innerHTML += renderizarPergunta(quiz.questions[i]);
+    }
 }
 
 function renderizarTituloDoQuiz (quiz) {
     return `
         <header class="titulo-do-quiz">
             <img src=${quiz.image} />
-            <h1>${quiz.titulo}</h1>
+            <h1>${quiz.title}</h1>
         </header>
+    `;
+}
+
+function renderizarPergunta (pergunta) {
+    let respostasHTML = "";
+    let arrRespostas = pergunta.answers;
+    arrRespostas.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < arrRespostas.length; i++) {
+        respostasHTML += `
+            <figure>
+                <img src=${arrRespostas[i].image} />
+                <figcaption>
+                    ${arrRespostas[i].text}
+                </figcaption>
+            </figure>
+        `;
+    }
+    return `
+        <section class="pergunta-container">
+            <header style="background-color: ${pergunta.color}">
+                ${pergunta.title}
+            </header>
+            <div>
+                ${respostasHTML}
+            </div>
+        </section>
     `;
 }
