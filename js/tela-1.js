c7bf3d6fa3eb2b9284d67de275059a402a6c7c07
Dimitri <dimitri@gmail.com>
2022-04-23 12:51:29 -0300

    Consertei o botão de adicionar outro quizz dps de ja ter quizzes do usuario

--- js/tela-1.js
+++ js/tela-1.js
@@ -1,83 +1,84 @@
 
-let arrayQuizzes = [];
-
 function carregarTela1 () {
     limparMain();
+    botaoCriarQuizz();
     resetarScroll();
-
-    const promessa = axios.get(API_URL);
-    promessa.then(function (resposta) {
-        arrayQuizzes = resposta.data;
-        carregarQuizzesDoUsuario();
-        carregarQuizzesDosOutros();
-    });
 }
 
-
-
-// Funções relacionadas a carregar os quizzes da seção de quizzes criados pelo usuário.
-function carregarQuizzesDoUsuario () {
+function foicriada(quizzes){
     const aux = JSON.parse(localStorage.getItem("IDS"));
-    if (aux === null) {
-        botaoCriarQuizz();
+    for(let i=0;i<aux.length;i++){
+        if(quizzes.id == aux[i].id){
+            console.log("entrou")
+            return true;
+        }
     }
-    else {
-        const quizzes = arrayQuizzes.filter(criadoPeloUsuario);
-        const qUser = renderizarQuizzesUsuario(quizzes);
-        CarregarUser(qUser);
-    }
-}
-
-function botaoCriarQuizz(){
-    MAIN.innerHTML = `
-        <div class = "CriarQuizzouQuizzCriados">
-            <p>Você não criou nenhum</br> quizz ainda :(</p>
-            <button class="botaoCriarQuizz" onclick="carregaTela3()">
-                <p>Criar Quizz</p>
-            </button>
-        </div>
-    `;
+    return false;
 }
 
 function CarregarUser(elemento){
-    MAIN.innerHTML += `
+    MAIN.innerHTML=`
                 <div class="user">
-                <div class="head"><header>Seus Quizzes</header>  <ion-icon name="add-circle" onclick="carregaTela3()"></ion-icon> </div>
+                <div class="head"><header>Seus Quizzes</header>  <ion-icon class="addNewQuizz" name="add-circle" onclick="carregaTela3()"></ion-icon> </div>
                 <div>${elemento}</div>
                 </div>
-            `;
+            `
+            carregarQuizzes();
+}
+
+function botaoCriarQuizz(){
+    const aux = JSON.parse(localStorage.getItem("IDS"));
+    if(aux === null){
+        MAIN.innerHTML = `
+            <div class = "CriarQuizzouQuizzCriados">
+                <p>Você não criou nenhum</br> quizz ainda :(</p>
+                <button class="botaoCriarQuizz" onclick="carregaTela3()">
+                    <p>Criar Quizz</p>
+                </button>
+            </div>
+        `;
+    }
+    else{
+        const promessa = axios.get(API_URL);
+        promessa.then(function (resposta) {
+            arrayQuizzes = resposta.data;
+            const quizzes = arrayQuizzes.filter(foicriada);
+            console.log(quizzes)
+            const qUser = renderizarQuizzesUsuario(quizzes);
+            CarregarUser(qUser);
+        });
+    }
 }
 
 function renderizarQuizzesUsuario (quizzes) {
     let quizzesHTML = "";
     console.log(quizzes)
     for (let i = 0; i < quizzes.length; i++) {
-        quizzesHTML += gerarQuizUsuarioHTML(quizzes[i]);
+        quizzesHTML += gerarQuizHTML(quizzes[i]);
     }
     return quizzesHTML;
 }
 
-function criadoPeloUsuario (quiz) {
+//Nova função para filtrar os Quizzes q NÃO foram feitos pelo usuario.
+function NfoiFeito(quizzes){
     const aux = JSON.parse(localStorage.getItem("IDS"));
-    if (aux === null) {
-        return false;
-    }
-
-    for (let i = 0; i < aux.length; i++) {
-        if (quiz.id == aux[i].id) {
+    for(let i=0;i<aux.length;i++){
+        if(quizzes.id != aux[i].id){
+            console.log("entrou")
             return true;
         }
     }
     return false;
 }
-// Funções relacionadas a carregar os quizzes da seção de quizzes criados pelo usuário.
-
-
-
 // Funções relacionadas a carregar os quizzes da seção de todos os quizzes.
-function carregarQuizzesDosOutros () {
-    const quizzes = arrayQuizzes.filter((quiz) => !criadoPeloUsuario(quiz));
-    renderizarQuizzes(quizzes);
+function carregarQuizzes () {
+    const promessa = axios.get(API_URL);
+
+    promessa.then(function (resposta) {
+        arrayQuizzes = resposta.data;
+        const quizzes = arrayQuizzes.filter(NfoiFeito);
+        renderizarQuizzes(quizzes);
+    });
 }
 
 function renderizarQuizzes (quizzes) {
@@ -105,22 +106,37 @@ function gerarQuizHTML (quiz) {
         </figure>
     `;
 }
-
-function gerarQuizUsuarioHTML (quiz) {
-    return `
-        <figure onclick="carregarTela2(${quiz.id})">
-            <img src=${quiz.image} />
-            <figcaption>
-                ${quiz.title}
-            </figcaption>
-            <div>
-                <ion-icon name="create-outline"></ion-icon>
-                <ion-icon name="trash-outline"></ion-icon>
-            </div>
-        </figure>
-    `;
-}
 // Funções relacionadas a carregar os quizzes da seção de todos os quizzes.
+
+
+
+// Lida com a serialização no localStorage das quizzes criads pelo usuário.
+function salvarQuizzesCriados (arr) {
+    const dadoSerializado = JSON.stringify(arr);
+    localStorage.setItem("quizzesCriados", dadoSerializado);
+}
+
+function pegarQuizzesCriados () {
+    const dado = JSON.parse(localStorage.getItem("quizzesCriados"));
+    if (dado === null) {
+        return false;
+    }
+    return JSON.parse(dadoSerializado);
+}
+
+function possuiSalvo (id) {
+    const arr = pegarQuizzesCriados();
+    if (arr.length === 0) {
+        return false;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === id) {
+            return true;
+        }
+    }
+    return false;
+}
+// Lida com a serialização no localStorage das quizzes criads pelo usuário.
 
 
 
